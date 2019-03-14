3a776f4bb069916d5a9b17c0644747aa0a6e53ad
Paul M Edwards <pauledwards@gmail.com>
2019-03-13 21:01:38 -0700

    Final tested implementation featuring support for both positive and negative Fibonacci sequences.

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int input = 0;
+            if (args.Length > 0) {
+                input = Int32.Parse(args[0]);
+                Console.WriteLine(string.Format("Lists Fibonacci sequence values beginning at 0 and proceeding to, but not past, the target value: {0}", input));
+                Console.WriteLine();
+
+                fibonacci(input);
+            } else {
+                Console.WriteLine("No target value specified... Please try again, providing a whole number (integer) as the command argument.\n");
+            }
+
+            void fibonacci(int max)
+            {
+                int x = 0;
+                int y = 1;
+
+                if (max < 0) {
+                    Console.WriteLine("Operating in Negative mode.\n");
+
+                    while ((0 - Math.Abs(x)) >= max) {
+                        Console.WriteLine(x);
+
+                        int z = x;  //Keep track of last value
+                        x = y - z;  //Subtract next value from last value
+                        y = z;      //Restore saved last value
+                    }
+                } else if (max > 0) {
+                    Console.WriteLine("Operating in Positive mode.\n");
+
+                    while (x <= max) {
+                        Console.WriteLine(x);
+
+                        int z = x;  //Keep track of last value
+                        x = z + y;  //Add last value to next value
+                        y = z;      //Restore saved last value
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Fibonacci
 Fibonacci example in C#
+
+## Usage:
+1. dotnet.exe .\Fibonacci.dll
+Results in message indicating an integer input argument is required.
+1. dotnet.exe .\Fibonacci.dll 8
+Generates and prints a positive Fibonacci sequence to 8.
+1. dotnet.exe .\Fibonacci.dll -8
+Generates and prints a negative Fibonacci sequence to -8.
